f391fb68ff5df82cdf37f83f330122d3765c0292
Admin <Admin@ADMIN>
2021-12-31 16:35:17 +0700

    Add project files.

---
+++ WebApplication4/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication4.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
